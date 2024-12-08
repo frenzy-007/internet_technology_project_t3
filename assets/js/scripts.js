cf45aaaef90584114590420041d7b28095569495
frenzy007 <menghourpech007@gmail.com>
2024-12-08 22:25:39 +0100

    commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,30 @@
 $(".menu-icon").on("click", function () {
-  $(".nav-menu").toggleClass("open");
+  const navMenu = $(".nav-menu");
+  const menuIcon = $(this).find("i");
+
+  // If the page is not at the top, scroll to the top and keep the menu open
+  if (window.scrollY !== 0) {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Smooth scroll to top
+    });
+
+    // Ensure the menu is open after scrolling
+    navMenu.addClass("open");
+    menuIcon.removeClass("fa-bars").addClass("fa-times");
+    $(this).attr("aria-label", "Close Menu");
+  } else {
+    // If the page is at the top, toggle the menu normally
+    navMenu.toggleClass("open");
+
+    if (navMenu.hasClass("open")) {
+      menuIcon.removeClass("fa-bars").addClass("fa-times");
+      $(this).attr("aria-label", "Close Menu");
+    } else {
+      menuIcon.removeClass("fa-times").addClass("fa-bars");
+      $(this).attr("aria-label", "Open Menu");
+    }
+  }
 });
 
 $(document).ready(function () {
@@ -31,3 +56,14 @@ $(document).ready(function () {
     $("#swappable-image").attr("src", images[currentIndex]);
   });
 });
+
+// Auto Adjustable Header height
+function adjustPadding() {
+  const logoContainer = document.querySelector(".container-logo");
+  const logoHeight = logoContainer.offsetHeight; // Get dynamic height
+  document.body.style.paddingTop = `${logoHeight}px`; // Apply as padding-top
+}
+
+// Run on page load and window resize
+window.addEventListener("load", adjustPadding);
+window.addEventListener("resize", adjustPadding);
