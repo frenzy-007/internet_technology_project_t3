ca32c7fe583b8c3564a0907fbe5042ba67853704
frenzy007 <menghourpech007@gmail.com>
2024-12-09 03:31:49 +0100

    commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -67,3 +67,281 @@ function adjustPadding() {
 // Run on page load and window resize
 window.addEventListener("load", adjustPadding);
 window.addEventListener("resize", adjustPadding);
+
+const items = [
+  {
+    title: "Lorem ipsum dolor sit amet.",
+    date: "20/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Consectetur adipisicing elit.",
+    date: "22/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Dolorem enim aspernatur error.",
+    date: "23/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Recusandae soluta amet sunt.",
+    date: "25/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Sed temporibus totam aliquam.",
+    date: "27/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  {
+    title: "Another article title.",
+    date: "28/02/2024",
+    img: "/assets/images/pexels-mohammedsuhail-2723091.jpg",
+  },
+  // Add more articles as needed
+];
+
+let currentPage = 1;
+let itemsPerPage = 6; // Default to 10 items per page if screen is 800px or larger
+if (window.innerWidth >= 1200) {
+  itemsPerPage = 12;
+} else if (window.innerWidth >= 900) {
+  itemsPerPage = 9;
+}
+
+// Adjust itemsPerPage based on screen size
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1200) {
+    itemsPerPage = 12;
+  } else if (window.innerWidth >= 900) {
+    itemsPerPage = 9;
+  }
+  currentPage = 1; // Reset to page 1 when screen size changes
+  displayItems();
+});
+
+function displayItems() {
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const pageItems = items.slice(startIndex, endIndex);
+
+  // Get the item container
+  const itemContainer = document.getElementById("item-container");
+
+  // Clear the container
+  itemContainer.innerHTML = "";
+
+  // Add the items to the container
+  pageItems.forEach((item) => {
+    const itemHTML = `
+        <article class="new-article-item margin-bottom-20">
+          <figure class="margin-bottom-0">
+            <img class="new-image" src="${item.img}" alt="New Image" />
+          </figure>
+          <div class="container-extra-info margin-bottom-20">
+            <div class="box-type-date background-color-orange">
+              <span>News</span>
+            </div>
+            <div class="box-type-date background-color-blue">
+              <span>${item.date}</span>
+            </div>
+          </div>
+          <div>
+            <a href=""><h3>${item.title}</h3></a>
+            <p>
+              ${item.title} Description text...
+            </p>
+          </div>
+        </article>
+    `;
+    itemContainer.innerHTML += itemHTML;
+  });
+
+  // Update the page number
+  const pageNumber = document.getElementById("page-number");
+  pageNumber.innerHTML = `Page ${currentPage} of ${Math.ceil(
+    items.length / itemsPerPage
+  )}`;
+
+  // Scroll to the top of the first item in the list
+  const firstItem = document.querySelector(".container-new-article-item");
+  if (firstItem) {
+    firstItem.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+function changePage(direction) {
+  if (
+    direction === "next" &&
+    currentPage < Math.ceil(items.length / itemsPerPage)
+  ) {
+    currentPage++;
+  } else if (direction === "prev" && currentPage > 1) {
+    currentPage--;
+  }
+  displayItems();
+}
+
+// Initial load
+displayItems();
